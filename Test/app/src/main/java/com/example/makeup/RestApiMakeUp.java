395fdf1cc8a4384f263e11484ddfa9f9f1ee3113
miskina <cookie20061@hotmail.fr>
2019-12-01 00:04:40 +0100

    succeed

--- Test/app/src/main/java/com/example/makeup/RestApiMakeUp.java
+++ Test/app/src/main/java/com/example/makeup/RestApiMakeUp.java
@@ -13,7 +13,7 @@ import retrofit2.http.GET;
 public interface RestApiMakeUp {
     @GET("products.json?product_category=lipstick&product_type=lipstick")
     Call<List<Lipstick>> getLipstickData();
-    @GET("products.json?product_category=liquid&product_type=foundation")
+    @GET("products.json?product_category=concealer&product_type=foundation")
     Call<List<Foundation>> getFoundationData();
     @GET("products.json?product_category=powder&product_type=blush")
     Call<List<Blush>> getBlushData();
---
+++ Test/app/src/main/java/com/example/makeup/view/SecondActivityBlush.java
@@ -0,0 +1,61 @@
+package com.example.makeup.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.makeup.model.Blush;
+import com.example.makeup.model.Foundation;
+import com.example.makeup.model.Lipstick;
+import com.example.test.R;
+import com.squareup.picasso.Picasso;
+
+public class SecondActivityBlush extends AppCompatActivity {
+
+    private static final String selection = "selection_label";
+
+    private TextView brand;
+    private TextView name;
+    private TextView price;
+    private TextView price_sign;
+    private TextView description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Blush fd = (Blush) getIntent().getSerializableExtra("Blush");
+
+
+        description = findViewById(R.id.desc);
+        brand = findViewById(R.id.brand);
+        name = findViewById(R.id.titre);
+        price = findViewById(R.id.prix);
+        price_sign = findViewById(R.id.prix_b);
+        ImageView image_link = findViewById(R.id.imageView);
+        Picasso.get().load(fd.getImage_link()).into(image_link);
+
+        description.setText(fd.getDescription());
+        brand.setText(fd.getBrand());
+        name.setText(fd.getName());
+        price.setText(fd.getPrice());
+        price_sign.setText(fd.getPrice_sign());
+
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
